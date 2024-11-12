ddf49058813061bc05450d090614850c7969c2dc
hima <19623185@std.stei.itb.ac.id>
2024-11-12 11:07:46 +0700

    style: color theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,9 @@
-"use client";
-
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    voiceflow: {
-      chat: {
-        load: (config: {
-          verify: { projectID: string };
-          url: string;
-          versionID: string;
-          render: { mode: string; target: HTMLElement };
-          autostart: boolean;
-        }) => void;
-      };
-    };
-  }
-}
+import { ChatBot } from "./_component/chat";
 
 export default function Home() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
-    script.type = "text/javascript";
-    script.async = true;
-    script.onload = () => {
-      window.voiceflow.chat.load({
-        verify: { projectID: "6731f3769f1273d401e4ddf7" },
-        url: "https://general-runtime.voiceflow.com",
-        versionID: "production",
-        render: {
-          mode: "embedded",
-          target: document.body,
-        },
-        autostart: false,
-      });
-    };
-    document.body.appendChild(script);
-  }, []);
-
-  return <main className="size-full"></main>;
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <ChatBot />
+    </main>
+  );
 }
